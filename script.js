5a6244855cc30142c9274c9ea9dbf3dd3f78c93f
1michael17 <94114865+1michael17@users.noreply.github.com>
2024-06-17 09:30:24 +0100

    Functions and black button event listener

--- script.js
+++ script.js
@@ -9,9 +9,38 @@ const btnColor = document.querySelector('.toColor');
 const inputColor = document.querySelector('#color');
 const dBtn = document.querySelectorAll('.dBtn');
 
+let numb = 10, color = 'black';
+btnBlack.setAttribute('id', 'activeBtn');
 
-function toActive(){
-    dBtn.forEach(e=>{
-    e.removeAttribute('id', 'dAct');
+function colorer(e, col){
+    e.addEventListener('mouseover', (e) => {
+        color = col;
+        e.target.style.backgroundColor = color; 
+        return color;
     });
 }
+
+function clearer(elem) {
+    elem.innerHTML = "";
+    elem.style.backgroundColor = '#fff';
+};
+
+function createBox(gBox, num) {
+    clearer(dBody);
+    for (let i = 1; i <= num * num; i++) {
+        const box = document.createElement('div');
+        box.style.flexBasis = `calc(100%/${numb})`;
+        box.classList.add('box');
+        gBox.appendChild(box);
+        inputNum.value = numb;
+    }
+    colorer(dBody, color);
+}
+
+createBox(dBody, numb);
+
+btnBlack.addEventListener('click', () => {
+    toActive();
+    btnBlack.setAttribute('id', 'activeBtn');
+    colorer(dBody, 'black');
+});
