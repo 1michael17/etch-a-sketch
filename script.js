677bebbbfb34fd36d134b4b692b4274c2080cec0
1michael17 <94114865+1michael17@users.noreply.github.com>
2024-06-17 07:21:30 +0100

    imported class and element

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-script.js
+const dBody = document.querySelector('.body');
+const btnRandom = document.querySelector('.random');
+const btnBlack = document.querySelector('.black');
+const btnClear = document.querySelector('.eraser');
+const btnReset = document.querySelector('.reset');
+const newNum = document.querySelector('.newNum');
+const inputNum = document.querySelector('#number');
+const btnColor = document.querySelector('.toColor');
+const inputColor = document.querySelector('#color');
+const dBtn = document.querySelectorAll('.dBtn');
